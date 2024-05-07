13302612f8b6d373fed83d6840f1af18858b667f
Sijia Wang <wangsijia0628@gmail.com>
2024-05-07 11:20:10 -0400

    formatting

--- tests/test_roadway.py
+++ tests/test_roadway.py
@@ -5,7 +5,7 @@ import pytest
 from cube_wrangler.roadway import Parameters, ModelRoadwayNetwork
 from network_wrangler import RoadwayNetwork
 
+
 def test_load(request):
-    """
-    """
+    """ """
     print("\n--Starting:", request.node.name)
